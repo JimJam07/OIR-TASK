60345f0d4bdb4dea73b17f7126e9ee25948a7729
sharan rajamanoharan <jimjam07@users.noreply.github.com>
2020-09-27 15:32:57 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,5 @@
+//for navbar
+document.getElementById("burger").addEventListener("click", (ev) => {
+  document.getElementById("burger").classList.toggle("open");
+  document.getElementById("links").classList.toggle("active");
+});
